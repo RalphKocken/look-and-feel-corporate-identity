c6f7df9af9a1569f6dd8de34303be7a6e290fab7
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2022-12-09 12:20:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-let dialog = document.querySelector(".dialogstekjeslijst-orchidee");
-let openDialog = document.querySelector(".stekjes-meer-info-button");
-let closeDialog = document.querySelector(".close-dialog-stekjes-button")
-
 
 let eenvoudig = document.querySelectorAll(".eenvoudig");
 let gemiddeld = document.querySelectorAll(".gemiddeld");
 let gevorderd = document.querySelectorAll(".gevorderd");
-
-// alle stekjes
 let stekjesTotaal = document.querySelector(".stekjeslijst")
 let eenvoudigButton = document.querySelector(".eenvoudigButton");
 let gemiddeldButton = document.querySelector(".gemiddeldButton");
@@ -15,31 +9,8 @@ let gevorderdButton = document.querySelector(".gevorderdButton");
 let laatalleszienButton = document.querySelector(".laatalleszienButton");
 let stekjeaanmeldenconfirm = document.querySelector(".stekje-aanmelden-button");
 
-var meldJePlantAanButton = document.getElementById('header-article > button')
-var plaatjeAanmeldenDialog = document.getElementById('dialogstekjeaanmelden')
-var section = document.querySelector('.stekjeslijst')
+// VOORBEELD VAN JUSTUS//
 
-plaatjeAanmeldenDialog.addEventListener('submit', function() {
-  var eenKloon = section.querySelector('.stekje').cloneNode(true)
-  console.log(eenKloon)
-  var ingevuldeNaam = document.querySelector('input[name="naampje"]')
-  var geselecteerdeFoto = document.querySelector('input[name="foto-stekje"]')
-
-  // Vergeet dit voor nu! :)
-  eenKloon.querySelector(".stekjes-meer-info-button").textContent = ingevuldeNaam.value
-  // Vergeet dit voor nu! :)
-  eenKloon.querySelector('img').src = window.URL.createObjectURL(geselecteerdeFoto.files[0])
-
-  plaatjeAanmeldenDialog.querySelector('form').reset()
-
-  // Nieuw! Een node toevoegen aan een andere node
-  section.append(eenKloon)
-
-  // En focus het net toegevoegde dingetje
-  eenKloon.scrollIntoView()
-
-  alert("Uw stekje is succesvol aangemeld.")
-})
 // easyPlants.forEach((plant) => {
 //   plant.addEventListener("click", plantHandler);
 // });
@@ -55,7 +26,7 @@ plaatjeAanmeldenDialog.addEventListener('submit', function() {
 // }
 
 
-
+// functie voor het filteren van de stekjes
 function removeAll(){
   eenvoudig.forEach(element => {
     element.style.display = "none";
@@ -68,7 +39,7 @@ function removeAll(){
   });
 }
 
-
+//functie voor het laten zien van alle stekjes
 function laatalleszien(){
   eenvoudig.forEach(element => {
     element.style.display = "flex";
@@ -94,6 +65,7 @@ function eenvoudigShow(){
   });
 }
 
+//gemiddeld knop
 gemiddeldButton.addEventListener("click", gemiddeldShow);
 
 function gemiddeldShow(){
@@ -101,12 +73,9 @@ function gemiddeldShow(){
   gemiddeld.forEach(element => {
     element.style.display = "flex";
   });
-
-  /*if(gemiddeld.length < 4){
-    stekjesTotaal.style.justifyContent = "center"
-  }*/
 }
 
+//gevorderd knop//
 gevorderdButton.addEventListener("click", gevorderdShow);
 
 function gevorderdShow(){
@@ -116,18 +85,30 @@ function gevorderdShow(){
   });
 }
 
+//laatalleszien knop
 laatalleszienButton.addEventListener("click", laatalleszienShow);
   function laatalleszienShow(){
     laatalleszien();
   }
 
-
-// KORTERE VERSIE EENVOUDIG BUTTON
-// eenvoudigButton.addEventListener("click", () => {
-//   removeAll();
-//   eenvoudig.forEach(element => {
-//     element.style.display = "flex";
-//   });
-// });
-
-
+// VOORBEELD VAN KRIJN
+// Functie voor het toevoegen van stekjes
+  var meldJePlantAanButton = document.getElementById('header-article > button')
+  var plaatjeAanmeldenDialog = document.getElementById('dialogstekjeaanmelden')
+  var section = document.querySelector('.stekjeslijst')
+  
+  plaatjeAanmeldenDialog.addEventListener('submit', function() {
+    var eenKloon = section.querySelector('.stekje').cloneNode(true)
+    console.log(eenKloon)
+    var ingevuldeNaam = document.querySelector('input[name="naampje"]')
+    var geselecteerdeFoto = document.querySelector('input[name="foto-stekje"]')
+  
+    eenKloon.querySelector(".stekjes-meer-info-button").textContent = ingevuldeNaam.value
+  
+    eenKloon.querySelector('img').src = window.URL.createObjectURL(geselecteerdeFoto.files[0])
+  
+    plaatjeAanmeldenDialog.querySelector('form').reset()
+  
+    section.append(eenKloon)
+  
+    eenKloon.scrollIntoView()
