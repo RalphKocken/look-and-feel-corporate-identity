8d1806390f3d2de3cac708b0a7d5aae5fe74d32b
RalphKocken <106448490+RalphKocken@users.noreply.github.com>
2022-12-09 12:15:56 +0100

    changes

--- script.js
+++ script.js
@@ -7,12 +7,39 @@ let eenvoudig = document.querySelectorAll(".eenvoudig");
 let gemiddeld = document.querySelectorAll(".gemiddeld");
 let gevorderd = document.querySelectorAll(".gevorderd");
 
+// alle stekjes
+let stekjesTotaal = document.querySelector(".stekjeslijst")
 let eenvoudigButton = document.querySelector(".eenvoudigButton");
 let gemiddeldButton = document.querySelector(".gemiddeldButton");
 let gevorderdButton = document.querySelector(".gevorderdButton");
 let laatalleszienButton = document.querySelector(".laatalleszienButton");
+let stekjeaanmeldenconfirm = document.querySelector(".stekje-aanmelden-button");
 
+var meldJePlantAanButton = document.getElementById('header-article > button')
+var plaatjeAanmeldenDialog = document.getElementById('dialogstekjeaanmelden')
+var section = document.querySelector('.stekjeslijst')
 
+plaatjeAanmeldenDialog.addEventListener('submit', function() {
+  var eenKloon = section.querySelector('.stekje').cloneNode(true)
+  console.log(eenKloon)
+  var ingevuldeNaam = document.querySelector('input[name="naampje"]')
+  var geselecteerdeFoto = document.querySelector('input[name="foto-stekje"]')
+
+  // Vergeet dit voor nu! :)
+  eenKloon.querySelector(".stekjes-meer-info-button").textContent = ingevuldeNaam.value
+  // Vergeet dit voor nu! :)
+  eenKloon.querySelector('img').src = window.URL.createObjectURL(geselecteerdeFoto.files[0])
+
+  plaatjeAanmeldenDialog.querySelector('form').reset()
+
+  // Nieuw! Een node toevoegen aan een andere node
+  section.append(eenKloon)
+
+  // En focus het net toegevoegde dingetje
+  eenKloon.scrollIntoView()
+
+  alert("Uw stekje is succesvol aangemeld.")
+})
 // easyPlants.forEach((plant) => {
 //   plant.addEventListener("click", plantHandler);
 // });
@@ -28,6 +55,7 @@ let laatalleszienButton = document.querySelector(".laatalleszienButton");
 // }
 
 
+
 function removeAll(){
   eenvoudig.forEach(element => {
     element.style.display = "none";
@@ -38,8 +66,6 @@ function removeAll(){
   gevorderd.forEach(element => {
     element.style.display = "none";
   });
-
-  document.getElementById("stekjeslijst").style.justifyContent = "center";
 }
 
 
@@ -52,9 +78,10 @@ function laatalleszien(){
   });
   gevorderd.forEach(element => {
     element.style.display = "flex";
-  });
 
-  document.getElemendById("stekjeslijst").style.justifyContent = "flex-start";
+    });
+
+ 
 }
 
 // eenvoudig knop
@@ -74,6 +101,10 @@ function gemiddeldShow(){
   gemiddeld.forEach(element => {
     element.style.display = "flex";
   });
+
+  /*if(gemiddeld.length < 4){
+    stekjesTotaal.style.justifyContent = "center"
+  }*/
 }
 
 gevorderdButton.addEventListener("click", gevorderdShow);
